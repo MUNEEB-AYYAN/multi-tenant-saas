c2489950572296ab544d2a5183b3986837dcf456
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2026-02-17 04:03:26 +0530

    cors

--- saas-backend/server.js
+++ saas-backend/server.js
@@ -17,7 +17,11 @@ app.get('/',(req,res)=>{
     res.send('hi from the server side')
 })
 app.use(cors({
-    origin: process.env.CLIENT_URL
+    origin: [
+        process.env.CLIENT_URL,
+        "http://localhost:5173"
+    ],
+    credentials: true
 }))
 
 app.use(express.json())
